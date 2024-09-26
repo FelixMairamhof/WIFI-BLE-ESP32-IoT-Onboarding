cea8887d4dde2b578e6717d2ba91f69f02f4a994
Felix Mairamhof <C_Hack7@kelagat.onmicrosoft.com>
2024-09-26 13:03:18 +0200

    Nimble v2

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,31 @@ class MyCallbacks : public NimBLECharacteristicCallbacks
       Serial.println(F("Received message: "));
       Serial.println(value.c_str());
 
+      // Check for scan command
+      if (value == "scan")
+      {
+        // Scan for Wi-Fi networks
+        Serial.println(F("Scanning for Wi-Fi networks..."));
+        int n = WiFi.scanNetworks();
+        String networks = "Available networks:\n";
+
+        for (int i = 0; i < n; ++i)
+        {
+          networks += String(i + 1) + ": " + WiFi.SSID(i) + " (" + WiFi.RSSI(i) + " dBm)\n";
+        }
+        
+        // Send the available networks back to the client
+        pCharacteristic->setValue(networks.c_str());
+        pCharacteristic->notify();
+      }
       // Check for connect command using compare
-      if (value.compare(0, 8, "connect:") == 0)
+      else if (value.compare(0, 8, "connect:") == 0)
       {
         // Extract credentials
         String credentials = String(value.substr(8).c_str());
         int delimiterIndex = credentials.indexOf(':'); // Find delimiter
-        if (delimiterIndex != -1) {
+        if (delimiterIndex != -1)
+        {
           String wifi_ssid = credentials.substring(0, delimiterIndex);
           String wifi_password = credentials.substring(delimiterIndex + 1);
 
@@ -34,16 +52,29 @@ class MyCallbacks : public NimBLECharacteristicCallbacks
           Serial.println(wifi_ssid);
 
           // Wait for connection
-          while (WiFi.status() != WL_CONNECTED) {
+          unsigned long startAttemptTime = millis();
+          while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) // 10 seconds timeout
+          {
             delay(500);
             Serial.print(F("."));
           }
-          Serial.println(F("\nConnected to Wi-Fi!"));
 
-          // Notify the client about the connection status
-          String response = "Connected to " + wifi_ssid;
-          pCharacteristic->setValue(response.c_str());
-          pCharacteristic->notify();
+          if (WiFi.status() == WL_CONNECTED)
+          {
+            Serial.println(F("\nConnected to Wi-Fi!"));
+            // Notify the client about the successful connection
+            String response = "Connected to " + wifi_ssid;
+            pCharacteristic->setValue(response.c_str());
+            pCharacteristic->notify();
+          }
+          else
+          {
+            Serial.println(F("\nFailed to connect to Wi-Fi."));
+            // Notify the client about the failure
+            String response = "Failed to connect to " + wifi_ssid;
+            pCharacteristic->setValue(response.c_str());
+            pCharacteristic->notify();
+          }
         }
       }
     }
